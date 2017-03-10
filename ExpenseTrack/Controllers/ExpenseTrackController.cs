3123ea4b4a0cd7f7ddb9bce12a48eda882834b48
tsailouis <pilitoko@gmail.com>
2017-03-10 17:30:31 +0800

    第二版 新增gridmvc

---
+++ ExpenseTrack/Controllers/ExpenseTrackController.cs
@@ -0,0 +1,34 @@
+﻿using ExpenseTrack.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExpenseTrack.Controllers
+{
+    public class ExpenseTrackController : Controller
+    {
+        // GET: ExpenseTrack
+        public ActionResult Index()
+        {
+            ViewBag.Message = "類別";
+            var items = new List<SelectListItem> {
+                new SelectListItem { Text = "支出", Value = "1" },
+                new SelectListItem { Text = "收入", Value = "2" }, };
+
+            ViewBag.CategoryItems = items;
+            return View();
+            
+        }
+
+        public ActionResult ExpenseList()
+        {
+            
+            ExpenseList t = new ExpenseList();
+            var myList = t.GetData();
+            return View(myList);
+
+        }
+    }
+}
---
+++ ExpenseTrack/Models/ViewModel/ExpenseTrackModels.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ExpenseTrack.Models
+{
+    public class ExpenseTrackModels
+    {
+     
+        //public string PayType { get; set; }
+        [DataType(DataType.Currency)]
+        [Required]
+        [Display(Name = "金額")]
+        public int Money { get; set; }
+
+        [Display(Name = "日期")]
+        public DateTime CreateDate { get; set; }
+
+        [DataType(DataType.MultilineText)]
+
+        [Display(Name = "備註")]
+        public string Description { get; set; }
+
+
+        public string PayType { get; set; }
+    }
+
+    public class ExpenseList
+    {
+        public List<ExpenseTrackModels> GetData()
+        {
+            try
+            {
+                List<ExpenseTrackModels> trackList = new List<ExpenseTrackModels>();
+                for (int i =1; i < 100; i++)
+                {
+                    ExpenseTrackModels expen = new ExpenseTrackModels();
+                    expen.PayType = (((i % 2 )==0) ? "支出" : "收入");
+                    expen.Money = i * 100;
+                    expen.CreateDate = DateTime.Now;
+                    expen.Description = "備註";
+                    trackList.Add(expen);
+                }
+                return trackList;
+            }
+            catch (Exception)
+            {
+                throw new NotImplementedException();
+            }
+
+        }
+    }
+}
