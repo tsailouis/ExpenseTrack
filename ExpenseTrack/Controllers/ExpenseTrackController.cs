8ff5334d4cb644a040153a755bbfdf82bec5b17f
tsailouis <pilitoko@gmail.com>
2017-03-16 11:59:49 +0800

    Use  ADO.NET to add data into expenselist.cshtml

--- ExpenseTrack/Controllers/ExpenseTrackController.cs
+++ ExpenseTrack/Controllers/ExpenseTrackController.cs
@@ -9,13 +9,14 @@ namespace ExpenseTrack.Controllers
 {
     public class ExpenseTrackController : Controller
     {
+        private ExpenseToDB dao = new ExpenseToDB();
         // GET: ExpenseTrack
         public ActionResult Index()
         {
             ViewBag.Message = "類別";
             var items = new List<SelectListItem> {
-                new SelectListItem { Text = "支出", Value = "1" },
-                new SelectListItem { Text = "收入", Value = "2" }, };
+                new SelectListItem { Text = "支出", Value = "0" },
+                new SelectListItem { Text = "收入", Value = "1" }, };
 
             ViewBag.CategoryItems = items;
             return View();
@@ -24,12 +25,14 @@ namespace ExpenseTrack.Controllers
 
         public ActionResult ExpenseList()
         {
-            
-            ExpenseList t = new ExpenseList();
-            var myList = t.GetData();
+            var ablist = dao.GetAllAccountBook();//viewmodel
+           // ExpenseList t = new ExpenseList();
+           // var myList = t.GetData();
           //  ViewData["ViewDataTest"] = myList;
-            return PartialView(myList);
+            return PartialView(ablist);
 
         }
+
+      
     }
 }
---
+++ ExpenseTrack/Models/ExpenseToDB.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace ExpenseTrack.Models
+{
+    public class ExpenseToDB
+    {
+        private string ConnectionString { get; set; }
+
+        public ExpenseToDB()
+        {
+            this.ConnectionString = WebConfigurationManager.ConnectionStrings["DBconnect"].ConnectionString;
+        }
+
+        /// <summary>
+        /// Gets all authors.
+        /// </summary>
+        /// <returns></returns>
+        public List<ExpenseTrackModels> GetAllAccountBook()
+        {
+            var result = new List<ExpenseTrackModels>();
+
+            const string sqlStatement = "Select  Amounttt,Dateee,Remarkkk,Categoryyy From AccountBook ";
+
+            using (var conn = new SqlConnection(this.ConnectionString))
+            using (var command = new SqlCommand(sqlStatement, conn))
+            {
+                command.CommandType = CommandType.Text;
+                command.CommandTimeout = 180;
+
+                if (conn.State != ConnectionState.Open) conn.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        //好處只有這裡弱型別,前面都用選的不會打錯
+                        var item = new ExpenseTrackModels
+                        {
+                            Money = int.Parse(reader["Amounttt"].ToString()),
+                            CreateDate = DateTime.Parse(reader["Dateee"].ToString()),
+                            Description = reader["Remarkkk"].ToString(),
+                            PayType= reader["Categoryyy"].ToString()
+                        };
+                        result.Add(item);
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
